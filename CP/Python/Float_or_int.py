9525a937ef573f7d6152dacfdcfc92b6a0d0dc4c
Pillai Bhavana Radhakrishna <118704821+master-ginger@users.noreply.github.com>
2023-09-11 09:37:23 +0530

    Update and rename 5day2.py to Float_or_int.py

--- CP/Python/Float_or_int.py
+++ CP/Python/Float_or_int.py
@@ -4,18 +4,18 @@ a=float(input())
 
 b=int(a)
 if a==b:
-    print("Integer",b)
+    print("int",b)
 else:
-    print("Float",b,"{:.3f}".format(a-b))
+    print("float",b,"{:.3f}".format(a-b))
 
 '''
 sample input1:
 234.000
 output1:
-Integer 234
+int 234
 
 sample input2:
 5334.578
 output2:
-Float 5334 0.578
+float 5334 0.578
 '''
